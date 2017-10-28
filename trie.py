27e50816d9fc4124c04b366c20dde7a089c88e38
Srinivas Reddy Thatiparthy <thatiparthysreenivas@gmail.com>
2017-10-28 23:37:56 +0530

    Add

--- trie.py
+++ trie.py
@@ -1,12 +1,12 @@
-
 class Node(object):
     def __init__(self, char, value):
         self.char = char
-        self.value = value 
+        self.value = value
+        self.prefix_max_length=0
         self.next = {}
 
     def __repr__(self):
-        return "Node({},{},{})".format(self.char, self.value, self.next)
+        return "Node({}, {}, {}, length:{})".format(self.char, self.value, self.next, self.prefix_max_length)
 
 class Trie(object):
     def __init__(self):
@@ -16,51 +16,32 @@ class Trie(object):
         next_pointers = self.root.next
         index = 0
         while(index<len(string)):
-            if string[index] in next_pointers:
-                next_pointers = next_pointers[string[index]].next
+            _char = string[index]
+            if _char in next_pointers:
+                next_pointers = next_pointers[_char].next                
    	    else:
                 if index + 1 == len(string):
-                    node = Node(string[index], value)
+                    node = Node(_char, value)
                 else:
-                    node = Node(string[index], None)
-                next_pointers[string[index]] = node
+                    node = Node(_char, None)
+                next_pointers[_char] = node
                 next_pointers = node.next
             index = index + 1
 
-    def print_max(self, next_pointers):
-        max_each = []
-        if len(next_pointers) == 0:
-            return -1
-        for pointer in next_pointers:
-            if next_pointers[pointer].value is None and not next_pointers[pointer].next :
-                return -1
-            else:
-                max_each.append(max(next_pointers[pointer].value, self.print_max(next_pointers[pointer].next)))
-        return max(max_each)
-    
-    def suggestions(self, string):
-        next_pointers = self.root.next
-        index = 0
-        while(index<len(string)):
-            if string[index] not in next_pointers:
-                index = index+1
-                break
-            else:
-                next_pointers = next_pointers[string[index]].next
-                index = index+1
-        return self.print_max(next_pointers)
-        
-
-############## MAIN ###############################
-
 trie = Trie()
+"""
 times, input_strings = map(int, raw_input().split())
 for _ in range(times):
     char, value = raw_input().split()
     trie.insert(char, int(value))
-#trie.insert('hackerrank', 9)
-#print trie.suggestions('hacker')
+"""
+trie.insert('hackerrank', 9)
+trie.insert('hackerrank', 9)
+trie.insert('hackerrank', 9)
+print trie.suggestions('hacker')
+"""
 for _ in range(input_strings):
     data = raw_input()
     trie.suggestions(data)
     
+"""
